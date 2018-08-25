d21b474dad4eb9c747d3d5c57eec73dbb5d27040
sanketsharma98 <sanketsharma98@gmail.com>
2018-08-24 22:08:05 -0700

    fibonacci_mem.java
    Find n'th term of Fibonacci using Memoization

---
+++ fibonacci_mem.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class fibonacci_mem{
+    long[] tm=new long[100];
+    fibonacci_mem()
+    {
+        Arrays.fill(tm,-1);
+        tm[0]=0;
+        tm[1]=1;
+    }
+
+    long fib(int n)
+    {
+      if(n<2)
+        return n;
+      else
+      {
+        if((tm[n-1]!=-1)&&(tm[n-2]!=-1))
+        {
+          tm[n]=tm[n-1]+tm[n-2];
+          return tm[n];
+        }
+        else if((tm[n-1]==-1)&&(tm[n-2]!=-1))
+                return (fib(n-1)+tm[n-2]);
+        else
+            return (fib(n-1)+fib(n-2));
+      }
+
+    }
+
+public static void main(String[] args){
+
+  Scanner sc=new Scanner(System.in);
+  System.out.println("Which term do you want to find from Fibonacci Series ?");
+  int term=sc.nextInt();
+  fibonacci_mem fb=new fibonacci_mem();
+  System.out.println("The Term is "+fb.fib(term));
+}
+}
